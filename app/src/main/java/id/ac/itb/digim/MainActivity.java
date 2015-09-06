e0a750e91e0d615b7d95618ff7c3a2dcef3a45e2
Mamat Rahmat <coderbodoh@gmail.com>
2015-09-06 18:06:45 +0700

    add seekbar

--- app/src/main/java/id/ac/itb/digim/MainActivity.java
+++ app/src/main/java/id/ac/itb/digim/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,14 +19,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int MAX_VALUE_SEEKBAR = 16777215;
+
     String mCurrentPhotoPath;
+    ImageView image;
+    SeekBar seekBar;
+    int progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.mCapturedImage);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(MAX_VALUE_SEEKBAR);
+        seekBar.setOnSeekBarChangeListener(this);
     }
 
     public void onCaptureImage(View view) {
@@ -40,7 +51,6 @@ public class MainActivity extends ActionBarActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView image = (ImageView) findViewById(R.id.mCapturedImage);
             image.setImageBitmap(imageBitmap);
         }
     }
@@ -60,5 +70,24 @@ public class MainActivity extends ActionBarActivity {
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = "file:" + image.getAbsolutePath();
         return image;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+        progress = progressValue;
+
+        // for testing purpose
+        // TODO change to Image Equalization
+        image.setColorFilter(-1*progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
