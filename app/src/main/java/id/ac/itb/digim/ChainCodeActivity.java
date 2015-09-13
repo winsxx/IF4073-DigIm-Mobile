5c52ab9d1f819085971cbb400fe393febf18a9fe
afik <k.afis3@rocketmail.com>
2015-09-13 14:23:29 +0700

    add chaincode activity

---
+++ app/src/main/java/id/ac/itb/digim/ChainCodeActivity.java
@@ -0,0 +1,59 @@
+package id.ac.itb.digim;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.net.URI;
+
+
+public class ChainCodeActivity extends ActionBarActivity {
+
+    private static final int RESULT_LOAD_IMG = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chain_code);
+    }
+
+    public void browseGallery(View view) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK &&
+                    data != null) {
+                Uri dataResult = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(dataResult, filePath, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePath[0]);
+                String imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView im = (ImageView) findViewById(R.id.imageView);
+                im.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+            } else {
+                Toast.makeText(this, "Gambar belum dipilih", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception ex){
+            Toast.makeText(this, "Terjadi kesalahan", Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/id/ac/itb/digim/EqualizeActivity.java
@@ -0,0 +1,106 @@
+package id.ac.itb.digim;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import id.ac.itb.digim.analytics.equalizer.GreyscaleCumulativeEqualizer;
+import id.ac.itb.digim.common.ImageMatrix;
+import id.ac.itb.digim.common.color.GreyscaleColor;
+import id.ac.itb.digim.converter.ImageConverter;
+
+
+public class EqualizeActivity extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int MAX_VALUE_SEEKBAR = 100;
+
+    String mCurrentPhotoPath;
+    ImageView image;
+    SeekBar seekBar;
+    int progress;
+
+    ImageMatrix<GreyscaleColor> mGreyscaleImageMatrix;
+    GreyscaleCumulativeEqualizer greyscaleCumulativeEqualizer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equalize);
+
+        image = (ImageView) findViewById(R.id.mCapturedImage);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(MAX_VALUE_SEEKBAR);
+        seekBar.setOnSeekBarChangeListener(this);
+    }
+
+    public void onCaptureImage(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mGreyscaleImageMatrix = ImageConverter.toGreyscaleMatrix(imageBitmap);
+            imageBitmap = ImageConverter.toBitmap(mGreyscaleImageMatrix);
+            image.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "DigIm_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+        progress = progressValue;
+        float progressReal = (float)progress / MAX_VALUE_SEEKBAR;
+
+        // for testing purpose MAX_VALUE_SEEKBAR
+        if(greyscaleCumulativeEqualizer == null && mGreyscaleImageMatrix != null){
+            greyscaleCumulativeEqualizer = new GreyscaleCumulativeEqualizer(mGreyscaleImageMatrix);
+        }
+        if(greyscaleCumulativeEqualizer != null){
+            mGreyscaleImageMatrix = greyscaleCumulativeEqualizer.getScaledEqualizedImageMatrix(progressReal);
+            image.setImageBitmap(ImageConverter.toBitmap(mGreyscaleImageMatrix));
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
