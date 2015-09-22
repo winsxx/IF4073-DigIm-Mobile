f5c208b0fe4e79eed4696923159769b383b1dd0f
Winson Waisakurnia <13512071@std.stei.itb.ac.id>
2015-09-22 23:33:44 +0700

    Add otsu

--- app/src/main/java/id/ac/itb/digim/common/converter/ImageConverter.java
+++ app/src/main/java/id/ac/itb/digim/common/converter/ImageConverter.java
@@ -45,7 +45,7 @@ public class ImageConverter {
             for(int j=0; j<width; j++){
                 GreyscaleColor grey = greyscaleColorImageMatrix.getPixel(i, j);
                 BinaryColor binary = new BinaryColor();
-                if (grey.getGrey() > 125) {
+                if (grey.getGrey() > otsuTresholder(greyscaleColorImageMatrix)) {
                     binary.setBinaryColor(BinaryColorType.WHITE);
                 } else {
                     binary.setBinaryColor(BinaryColorType.BLACK);
@@ -69,6 +69,52 @@ public class ImageConverter {
         }
 
         return bitmap;
+    }
+
+    private static int otsuTresholder(ImageMatrix<GreyscaleColor> imageMatrix){
+        // Calculate histogram
+        int[] histData = new int[256];
+        for (int i = 0; i < imageMatrix.getHeight(); ++i) {
+            for (int j = 0; j < imageMatrix.getWidth(); ++j) {
+                histData[imageMatrix.getPixel(i, j).getGrey()]++;
+            }
+        }
+
+        int total = imageMatrix.getHeight() * imageMatrix.getWidth();
+
+        float sum = 0;
+        for (int t=0 ; t<256 ; t++) sum += t * histData[t];
+
+        float sumB = 0;
+        int wB = 0;
+        int wF = 0;
+
+        float varMax = 0;
+        int threshold = 0;
+
+        for (int t=0 ; t<256 ; t++) {
+            wB += histData[t];               // Weight Background
+            if (wB == 0) continue;
+
+            wF = total - wB;                 // Weight Foreground
+            if (wF == 0) break;
+
+            sumB += (float) (t * histData[t]);
+
+            float mB = sumB / wB;            // Mean Background
+            float mF = (sum - sumB) / wF;    // Mean Foreground
+
+            // Calculate Between Class Variance
+            float varBetween = (float)wB * (float)wF * (mB - mF) * (mB - mF);
+
+            // Check if new maximum found
+            if (varBetween > varMax) {
+                varMax = varBetween;
+                threshold = t;
+            }
+        }
+
+        return threshold;
     }
 
 }
