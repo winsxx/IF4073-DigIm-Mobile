900c4bb6f499369901c650458e97782ab6bad78f
Winson Waisakurnia <13512071@std.stei.itb.ac.id>
2015-09-27 17:13:02 +0700

    Add log flood fill

--- app/src/main/java/id/ac/itb/digim/common/Fill/FloodFill.java
+++ app/src/main/java/id/ac/itb/digim/common/Fill/FloodFill.java
@@ -1,6 +1,7 @@
 package id.ac.itb.digim.common.fill;
 
 import android.graphics.Point;
+import android.util.Log;
 
 import java.util.Stack;
 
@@ -9,45 +10,46 @@ import id.ac.itb.digim.common.color.BinaryColor;
 
 public class FloodFill {
 
-    public static ImageMatrix<BinaryColor> BinaryFloodFill(int row, int col, BinaryColor color,
-                                                           ImageMatrix<BinaryColor> image)
-    {
-        //System.out.println("Filling........");
+    public static ImageMatrix<BinaryColor> BinaryFloodFill(int row, int col, BinaryColor backgroundColor,
+                                                           ImageMatrix<BinaryColor> image) {
+        Log.i("[FLOOD_FILL][BINARY_FLOOD_FILL]", "Flood fill from row:" + row + " , col:" + col + ", background color:" + backgroundColor.getBinaryColor());
+        int counter = 0;
         ImageMatrix<BinaryColor> result = new ImageMatrix<>(image);
 
         BinaryColor now;
         Stack<Point> stackfill = new Stack<>();
-        Point posisi = new Point(row,col);
+        Point posisi = new Point(row, col);
         stackfill.push(posisi);
 
-        while(!stackfill.empty()){
+        while (!stackfill.empty()) {
             posisi = stackfill.pop();
+            counter++;
             now = result.getPixel(posisi.x, posisi.y);
-            result.setPixel(posisi.x, posisi.y, color);
+            result.setPixel(posisi.x, posisi.y, backgroundColor);
 
-            if(color.getBinaryColor() != now.getBinaryColor()) {
-                if (posisi.x-1 >= 0) {
-                    Point po = new Point(posisi.x-1,posisi.y);
+            if (backgroundColor.getBinaryColor() != now.getBinaryColor()) {
+                if (posisi.x - 1 >= 0) {
+                    Point po = new Point(posisi.x - 1, posisi.y);
                     stackfill.push(po);
                 }
 
-                if (posisi.x+1 < image.getHeight()) {
-                    Point po2 = new Point(posisi.x+1,posisi.y);
+                if (posisi.x + 1 < image.getHeight()) {
+                    Point po2 = new Point(posisi.x + 1, posisi.y);
                     stackfill.push(po2);
                 }
 
-                if (posisi.y+1 < image.getWidth()) {
-                    Point po3 = new Point(posisi.x,posisi.y+1);
+                if (posisi.y + 1 < image.getWidth()) {
+                    Point po3 = new Point(posisi.x, posisi.y + 1);
                     stackfill.push(po3);
                 }
 
-                if (posisi.y-1 >= 0) {
-                    Point po4 = new Point(posisi.x,posisi.y-1);
+                if (posisi.y - 1 >= 0) {
+                    Point po4 = new Point(posisi.x, posisi.y - 1);
                     stackfill.push(po4);
                 }
             }
         }
-
+        Log.i("[FLOOD_FILL][BINARY_FLOOD_FILL]", "Flood fill from row:" + row + " , col:" + col + ", pixel removed:" + counter);
         return result;
     }
 
