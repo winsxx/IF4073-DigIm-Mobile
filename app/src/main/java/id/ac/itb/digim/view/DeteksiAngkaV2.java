7b8ea0180fd8e8db3c53b593ba3c428a830618f1
Mamat Rahmat <coderbodoh@gmail.com>
2015-10-05 08:17:35 +0700

    fix layout activity_deteksi_angka_v2

--- app/src/main/java/id/ac/itb/digim/view/DeteksiAngkaV2.java
+++ app/src/main/java/id/ac/itb/digim/view/DeteksiAngkaV2.java
@@ -4,15 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import id.ac.itb.digim.R;
 
 public class DeteksiAngkaV2 extends ActionBarActivity {
 
+    ImageView image;
+    ImageView fiveSquareImage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deteksi_angka_v2);
+        image = (ImageView) findViewById(R.id.bitmapView);
+        image = (ImageView) findViewById(R.id.fiveSquareImageView);
     }
 
 
